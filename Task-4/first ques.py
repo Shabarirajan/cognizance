27de61ddcf6ba97c6205f0acb40f1aa477fafa28
Shabarirajan <99632244+Shabarirajan@users.noreply.github.com>
2022-02-23 23:04:08 +0530

    Delete Task-4 directory

--- Task-4/first ques.py
+++
@@ -1,7 +0,0 @@
-# Q1.Write a program to perform addition on two user input numbers.
-x=int(input("Enter a first number:")); 
-y=int(input("Enter a second number:"));
-sum=x+y
-print("")
-print("Sum of the two user input numbers: ",sum)
-
--- Task-4/second ques.py
+++
@@ -1,6 +0,0 @@
-#Q2.Write a program to accept a string from the user and display characters, 
-#that are present at an even index number.
-string = input('Enter the string:')
-chars = string[::2]
-print(chars)
-
